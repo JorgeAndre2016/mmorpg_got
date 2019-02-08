170b49d80cdd26247868ff0c11a5576888ddea2b
Jorge <jorge_aquino@hotmail.com.br>
2019-02-08 00:49:55 -0200

    criando método para destruir session

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,13 @@
-module.exports.jogo = function(application, req, res){
-    res.render('jogo');
+module.exports.jogo = function (application, req, res) {
+    if (req.session.autorizado) {
+        res.render('jogo');
+    } else {
+        res.redirect('/');
+    }
+}
+
+module.exports.sair = function (application, req, res) {
+    req.session.destroy(function(err, resp){
+        res.redirect('/');
+    });
 }
