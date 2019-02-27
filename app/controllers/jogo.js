60c89dc407d37a01630b468db8f0fad8f2a7638c
Jorge <jorge_aquino@hotmail.com.br>
2019-02-26 23:58:40 -0300

    insert chamada iniciaJogo no controller

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -5,12 +5,11 @@ module.exports.jogo = function (application, req, res) {
     };
 
     var usuario = req.session.usuario;
+    var casa = req.session.casa;
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(usuario);
-
-    res.render('jogo', { img_casa: req.session.casa });
+    JogoDAO.iniciaJogo(res, usuario, casa);
 }
 
 module.exports.sair = function (application, req, res) {
