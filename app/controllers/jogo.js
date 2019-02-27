5dd7ec6113503c9a5a58733391b9d46f840ae44c
Jorge <jorge_aquino@hotmail.com.br>
2019-02-26 23:42:58 -0300

    modificando lógica de acesso

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,13 +1,20 @@
 module.exports.jogo = function (application, req, res) {
-    if (req.session.autorizado) {
-        res.render('jogo', {img_casa: req.session.casa});
-    } else {
-        res.redirect('/');
-    }
+    if (req.session.autorizado !== true) {
+        res.send("Usuário precisa fazer login");
+        return;
+    };
+
+    var usuario = req.session.usuario;
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    JogoDAO.iniciaJogo(usuario);
+
+    res.render('jogo', { img_casa: req.session.casa });
 }
 
 module.exports.sair = function (application, req, res) {
-    req.session.destroy(function(err, resp){
+    req.session.destroy(function (err, resp) {
         res.redirect('/');
     });
 }
