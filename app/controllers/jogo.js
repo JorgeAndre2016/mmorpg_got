1c20e538560e2bde6b75fe027e8511e60ba9cb49
Jorge <jorge_aquino@hotmail.com.br>
2019-02-27 23:23:55 -0300

    insert validação de erros

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -4,6 +4,11 @@ module.exports.jogo = function (application, req, res) {
         return;
     };
 
+    var comando_invalido = 'N';
+    if(req.query.comando_invalido == 'S'){
+        comando_invalido = 'S';
+    }
+    
     var usuario = req.session.usuario;
     var casa = req.session.casa;
     var connection = application.config.dbConnection;
@@ -24,4 +29,20 @@ module.exports.suditos = function (application, req, res) {
 
 module.exports.pergaminhos = function (application, req, res) {
     res.render('pergaminhos', {validacao: {}});
+}
+
+module.exports.order_acao_sudito = function (application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('acao', "Ação deve ser informada").notEmpty();
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.redirect('jogo?comando_invalido=S');
+        return;
+    };
+
+    res.send('tudo ok');
 }
